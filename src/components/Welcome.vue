d1aeb885516b6de5eaa8099d4a266540c8da0d92
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2025-01-17 22:36:31 +0100

    Le problème d'affichage vient de la route qui est encore undefined lorsque la vue Welcome est affichée. Tentons plusieurs pistes de résolution

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -3,19 +3,70 @@ import Test from "../components/Test.vue";
 </script>
 
 <template>
-        <div id="message">
-        <h3 id="message" class="cormorant-garamond-semibold" >Vous êtes sur Medic'Help Calendar. Le calendrier numérique pour vos prises de médicaments, vos renouvellements d'ordonnances et tout ce qu'il vous faut ;)</h3>
-         <!-- <Test/> -->
+        <div id="welcome">
+            <h3 class="cormorant-garamond-semibold" >Vous êtes sur Medic'Help Calendar. Le calendrier numérique pour vos prises de médicaments, vos renouvellements d'ordonnances et tout ce qu'il vous faut ;)</h3>
         </div>
 </template>
 
-<style scoped>
-#message {
-    /* padding-left: 10vw;
-    padding-right: 10vw;
-    padding-top: 40vh;
-    padding-bottom: 40vh;
-    margin: 0px 10px 50px 10px; */
-    /*je met un background tranparent parce que je n'ai aps réussi à retirer l'heritage de style du parent (Home) vers l'enfant (Welcome) */
-}
+<style>
+.cormorant-garamond-light {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 300;
+    font-style: normal;
+  }
+  .cormorant-garamond-regular {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 400;
+    font-style: normal;
+  } 
+  .cormorant-garamond-medium {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 500;
+    font-style: normal;
+  }
+  .cormorant-garamond-semibold {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 600;
+    font-style: normal;
+  }
+  .cormorant-garamond-bold {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 700;
+    font-style: normal;
+  }
+  .cormorant-garamond-light-italic {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 300;
+    font-style: italic;
+  }
+  .cormorant-garamond-regular-italic {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 400;
+    font-style: italic;
+  }
+  .cormorant-garamond-medium-italic {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 500;
+    font-style: italic;
+  }
+  .cormorant-garamond-semibold-italic {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 600;
+    font-style: italic;
+  }
+  .cormorant-garamond-bold-italic {
+    font-family: "Cormorant Garamond", serif;
+    font-weight: 700;
+    font-style: italic;
+  }
+  
+
+
+/* @media (max-width: 1024px)
+{ 
+    body {
+height: 100%!important;
+overflow: auto!important;
+max-height: 100% !important;
+} */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,26 @@
 <script setup lang="ts" defer>
     import { useRoute } from "vue-router";
     import Welcome from "../components/Welcome.vue";
-    import Test from "../components/Test.vue"
-    import { computed } from "vue";
+    // import Test from "../components/Test.vue"
+    // import { computed } from "vue";
 
 
     const route = useRoute();
-
-//     Il faut utilsier soit une propriété computed (se renseigner, https://vuejs.org/guide/essentials/computed , https://vuejs.org/guide/typescript/composition-api#typing-computed )
-//     ou bien une condition?
-
-// Cette condition ci-dessous ne marche pas (<Welcome> est utilisé comme un type mais devrat être une vlaeur? Dois-je créer un type/une classe Welcome??)
-//     const displayed: String = (route.name === "home") ? <Welcome/> : <RouterView/>;
-
-// Sinon : 
-const isHomePage: Boolean = route.name === 'home';
+    //computed()
+    const isHomePage: Boolean = route.name === 'home';
+    console.log("route.name : ",route.name)
+    console.log("isHomePage = : ",isHomePage)
 
 </script>
 
 <template>
-
- <div id="homed">
-<!--Si j'appelle l'id "home", on dirait bien queVue confond avec le route.name car les liens ne marchent plus.-->
-
-    <Welcome v-if="isHomePage" />
-     <!-- <Welcome/> -->
-    <!-- problème de type??? -->
-
-      <RouterView v-else/>
-      <!-- ajouter ue condition dans le template pour afficher le h3 uniquement si la route active est la page d'accueil (/).
-      utiliser computed et la méthode useRoute de vue-router. -->
-  </div>
-
+    <div id="homed">
+        <!--Si j'appelle l'id "home", on dirait bien queVue confond avec le route.name car les liens ne marchent plus.-->
+        <Welcome/>
+        <!-- v-if=
+              "isHomePage -->
+        <RouterView/>
+    </div>
 </template>
 
  <style scoped>
@@ -43,11 +32,9 @@ const isHomePage: Boolean = route.name === 'home';
     padding: 0 5vw;
     /* border: 5px solid red; */
     /* l'axe principal est horinzontal (x) car main est en flex-direction: row */
-
     color: white;
     background: linear-gradient(0deg, #07c7e9 0%, #ffffff 100%);
     text-shadow: 4px 4px 5px rgba(0, 0, 0, 0.233);
     font-size: 25px;
-
 }
 </style>
